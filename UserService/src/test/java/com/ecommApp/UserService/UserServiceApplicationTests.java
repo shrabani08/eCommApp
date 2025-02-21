be2b5a7be2c61017d6ac0a64a0d20d02056c9b1d
shrabani <shrabani.mandal08@gmail.com>
2025-02-21 23:52:01 +0530

    1st commit

---
+++ UserService/src/test/java/com/ecommApp/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommApp.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
